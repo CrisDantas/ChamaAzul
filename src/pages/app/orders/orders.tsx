41208575a93f9852ed9f960a3b4cd99d5d8c60ef
CrisDantas <cristinadantas667@gmail.com>
2025-06-12 18:39:13 -0300

    inicio de pagina de pedidos

---
+++ src/pages/app/orders/orders.tsx
@@ -0,0 +1,27 @@
+import { ArrowRight, Search, X } from 'lucide-react'More actions
+import { Helmet } from 'react-helmet-async'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+export function Orders() {More actions
+  return (
+    <>
+      <Helmet title="Pedidos" />
+
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
+      </div>
+      <div className="space-y-2.5">
+        <form className="flex items-center gap-2">
+          <span className="text-sm font-semibold">Filtros:</span>
+          <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
+        </form>
