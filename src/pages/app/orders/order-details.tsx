ce7285c46daab8b5920c013c6a4dc1468c39c14a
CrisDantas <cristinadantas667@gmail.com>
2025-06-18 17:14:33 -0300

    Componente detlhes do pedido

---
+++ src/pages/app/orders/order-details.tsx
@@ -0,0 +1,76 @@
+import { DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+
+export function OrderDetails() {
+    return (
+        // container 1 
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle>Pedido: 1545121203</DialogTitle>
+                <DialogDescription>Detalhes do pedido</DialogDescription>
+            </DialogHeader>
+
+            {/* container 2  */}
+            <div className="space-y-6">
+                <Table>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell className="text-muted-foreground">Status</TableCell>
+                            <TableCell className="flex justify-end">
+                                <div className="flex items-center gap-2">
+                                    <span className="h-2 w-2 rounded-full bg-slate-400" />
+                                    <span className="font-medium text-muted-foreground">
+                                        Pendente
+                                    </span>
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="text-muted-foreground">Cliente</TableCell>
+                            <TableCell className="flex justify-end">cris dantas</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="text-muted-foreground">Telefone</TableCell>
+                            <TableCell className="flex justify-end">139999999</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="text-muted-foreground">Email</TableCell>
+                            <TableCell className="flex justify-end">@gmail</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="text-muted-foreground">realizado há</TableCell>
+                            <TableCell className="flex justify-end">há 15 minutos</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>Produto</TableHead>
+                            <TableHead className="text-right">Qtd</TableHead>
+                            <TableHead className="text-right">Preço</TableHead>
+                            <TableHead className="text-right">Subtotal</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>Pizza de calabresa</TableCell>
+                            <TableCell className="text-right">1</TableCell>
+                            <TableCell className="text-right"> R$ 20,00</TableCell>
+                            <TableCell className="text-right">R$ 20,00</TableCell>
+                        </TableRow>
+                    </TableBody>
+                    <TableFooter>
+                        <TableRow>
+                            <TableCell colSpan={3}>Total do pedido: </TableCell>
+                            <TableCell className="text-right font-medium">
+                                R$ 20,00
+                            </TableCell>
+                        </TableRow>
+                    </TableFooter>
+                </Table>
+            </div>
+        </DialogContent>
+
+    )
+}
--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,15 +1,24 @@
 import { Button } from "@/components/ui/button";
-import {  TableRow, TableCell } from "@/components/ui/table";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { TableRow, TableCell } from "@/components/ui/table";
 import { Search, ArrowRight, X } from "lucide-react";
+import { OrderDetails } from "./order-details";
 
 export function OrderTableRow() {
     return (
         <TableRow>
             <TableCell>
-                <Button variant="outline" size="sm">
-                    <Search className="h-3 w-3" />
-                    <span className="sr-only">Detalhes do pedido</span>
-                </Button>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button variant="outline" size="sm">
+                            <Search className="h-3 w-3" />
+                            <span className="sr-only">Detalhes do pedido</span>
+                        </Button>
+                    </DialogTrigger>
+                    {/* tabela secundaria com todos os detalhes do pedido que abre igual pop up */}
+                    <OrderDetails />
+                </Dialog>
+
             </TableCell>
             <TableCell className="font-mono text-xs font-medium">
                 821e78f7asdhdf128h
