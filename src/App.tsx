2d245788363927e1ae7afbac5eed59b4a062d448
CrisDantas <cristinadantas667@gmail.com>
2025-08-20 13:11:38 -0300

    config

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,18 @@ import { ThemeProvider } from "./components/theme/theme-provider"
 import { RouterProvider } from 'react-router-dom'
 import { router } from "./routes"
 import { Toaster } from 'sonner'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 function App() {
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme ">
       <div>
-        <RouterProvider router={router} />
-        < Toaster richColors />
+        <QueryClientProvider client={queryClient}>  
+          <RouterProvider router={router} />
+          < Toaster richColors />
+         </QueryClientProvider>
       </div>
     </ThemeProvider>
   )
